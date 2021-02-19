84e84515f68a0f806da5619ce3bd38281da1fe26
PoPaSeNa <popasena2@yandex.ru>
2021-02-19 19:12:23 +0300

    Что-то не то с показу по клику. Надо еще посидеть
    Signed-off-by: PoPaSeNa <popasena2@yandex.ru>

--- source/scripts/MainWindow.py
+++ source/scripts/MainWindow.py
@@ -1,5 +1,6 @@
 from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtCore import Qt
 
 
 # Класс главного окна
@@ -11,6 +12,9 @@ class MainWindow(QMainWindow):
         # Работать с ApiHandler удобнее в MainWindow
         self.static_api_handler = static_handler
         self.search_api_handler = search_handler
+
+        # Размеры QLabel для отображения карты. Понадобится для отслеживания кликов
+        self.MAP_RENDER_SIZE = (self.map_render.width(), self.map_render.height())
 
         self.set_map(self.static_api_handler.get_map())
         self.set_status(self.static_api_handler.get_status())
@@ -54,17 +58,19 @@ class MainWindow(QMainWindow):
         self.new_img()
 
     # Найти объект
-    def find_object(self):
-        text = self.input_object.text()
+    def find_object(self, point=False):
+        if not point:
+            text = self.input_object.text()
+        else:
+            text = point
         if text.replace(' ', '') != '':
             # Устанавливаем соединение с SearchApi
             self.search_api_handler.set_params(key=('text', text))
             self.search_api_handler.new_response()
             point = self.search_api_handler.get_point()
+            print(point)
             if point:
-                self.static_api_handler.set_params(key=('ll', point))
-                self.static_api_handler.set_params(key=('pt', point + ',pm2wtm'))
-                self.new_img()
+                self.set_new_object(point)
                 # Передаем ll в параметры для следуещего поиска
                 self.search_api_handler.set_params(key=('ll', point))
                 self.address_text.setPlainText('Адрес: \n' +
@@ -73,6 +79,12 @@ class MainWindow(QMainWindow):
                 self.address_text.setPlainText('Объект не найден')
         else:
             self.address_text.setPlainText('Объект не введен')
+
+    # Установить новый объект на карте
+    def set_new_object(self, point):
+        self.static_api_handler.set_params(key=('ll', point))
+        self.static_api_handler.set_params(key=('pt', point + ',pm2wtm'))
+        self.new_img()
 
     # Сброс найденного объекта
     def clear_object(self):
@@ -93,3 +105,42 @@ class MainWindow(QMainWindow):
         else:
             # Если адреса нет, то переводим CheckBox в False
             self.add_index.setCheckState(False)
+
+    # Событии мыши
+    def mousePressEvent(self, event):
+        # Позиция относительно self.map_render
+        pos = self.map_render.mapFromParent(event.pos())
+        pos = (pos.x(), pos.y())
+
+        if 0 <= pos[0] <= 600 and 0 <= pos[1] <= 450:
+            if event.button() == Qt.LeftButton:
+                # Левая конпки мыши
+                self.find_object_by_click(pos)
+            elif event.button() == Qt.RightButton:
+                # Правая конпки мыши
+                pass
+
+    # Найти объект по клику`
+    def find_object_by_click(self, pos):
+        # Координаты центра карты
+        coord = tuple(map(float, self.static_api_handler.params['ll'].split(',')))
+        # Охват карты
+        spn = tuple(map(float, self.static_api_handler.params['spn'].split(',')))
+        points = self.get_points_from_pos(pos, spn, coord)
+        print(points)
+        self.find_object(points)
+
+    # Получим долготу и широту на которую кликнули
+    def get_points_from_pos(self, pos, spn, coord):
+        # Преобразуем pos в pos относительно центра map_render
+        pos = tuple((pos[i] - self.MAP_RENDER_SIZE[i] // 2) * (1, -1)[i]
+                    for i in range(2))
+        print(pos)
+        # Рассчитаем на какие координаты по клику мы попали
+        # Преобразуем spn в долготу/широту за единицу на координатах
+        spn = tuple(spn[i] / (self.MAP_RENDER_SIZE[i] // 2) for i in range(2))
+        # Разница координат
+        spn = tuple(pos[i] * spn[i] for i in range(2))
+        print(spn)
+        # Возвращаем координаты по которым кликнули
+        return ','.join(reversed(tuple(str(round(coord[i] + spn[i], 6)) for i in range(2))))
